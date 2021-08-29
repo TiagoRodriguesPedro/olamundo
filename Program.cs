bb646b948bb63614c061ce4734cec764e442a5b6
Tiago Rodrigues <isatiago098@gmail.com>
2021-08-29 01:01:25 -0300

    resolução do exerciçio olamundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Ola usuario Tiago Rodrigues");
+        }
+    }
+}
